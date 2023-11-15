26ebfebc76410a9f712e5a97813b573143cc66e4
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-15 17:23:35 -0500

    Fix origins

--- api_bad_CORS/api/main.py
+++ api_bad_CORS/api/main.py
@@ -8,7 +8,7 @@ import os
 environment = os.environ.get("ENVIRONMENT", "vuln")
 origins = ["*"]
 if environment == "safe":
-    origins = ["http://bank.local", "https://bank.demo.stchepinsky.net"]
+    origins = ["bank.local", "bank.demo.stchepinsky.net"]
 
 class Item(BaseModel):
     dest: str

f0277c4b8cd21b9533bd7809012f97f41a360c6b
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-16 19:52:40 -0500

    Add semi safe and safe endpoints

--- api_ssrf/api/main.py
+++ api_ssrf/api/main.py
@@ -4,8 +4,9 @@ from pydantic import BaseModel
 from fastapi.openapi.utils import get_openapi
 import os
 import requests
-
-
+import socket
+from urllib.parse import urlparse
+import ipaddress
 
 
 origins = [
@@ -29,9 +30,49 @@ app.add_middleware(
 
 
 @app.get("/fetch/image")
-async def get_money(url:str):
+async def fetch(url:str):
     img = requests.get(url)
     if img.status_code != 200:
         raise HTTPException(status_code=404, detail="Image not found")
     return Response(content=img.content)
+
+@app.get("/samisafe/fetch/image")
+async def fetch_samisafe(url:str):
+    # refuse all private ip :
+    private_ip_startwith = ["10.", "172.16.", "192.168.", "127.", "169.254.", "100.64."]
+    if private_ip_startwith in url:
+        raise HTTPException(status_code=403, detail="Private IP not allowed")
+    img = requests.get(url)
+    if img.status_code != 200:
+        raise HTTPException(status_code=404, detail="Image not found")
+    return Response(content=img.content)
+        
+
+
+@app.get("/safe/fetch/image")
+async def fetch_safe(url:str):
+    try:
+        hostname = urlparse(url).hostname
+    except Exception as e:
+        print("Exception: ", e)
+        raise HTTPException(status_code=404, detail="Image not found")
+    print("hostname: ", hostname)
+    if hostname is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    ip = socket.gethostbyname(hostname)
+    print("ip: ", ip)
+    if ipaddress.ip_address(ip).is_private:
+        print("Private IP not allowed")
+        raise HTTPException(status_code=403, detail="Private IP not allowed")
+    img = requests.get(url)
+    if img.headers["Content-Type"] != "image/png" and img.headers["Content-Type"] != "image/jpeg" and img.headers["Content-Type"] != "image/gif" and img.headers["Content-Type"] != "image/webp" and img.header["Content-Type"] != "image/mp4":
+        print("Not an image extension")
+        raise HTTPException(status_code=404, detail="Image not found")
+    if img.status_code != 200:
+        print("Image not found")
+        raise HTTPException(status_code=404, detail="Image not found")
+    return Response(content=img.content, media_type="image/png")
+    
+
+    
 

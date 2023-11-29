cfc77ff1e35a0c99321f39d766850afcf9d0aacf
adrien <adri.bonnivard@gmail.com>
2023-11-28 19:37:26 -0500

    change login func badauth

--- api_badauth/api/main.py
+++ api_badauth/api/main.py
@@ -57,18 +57,30 @@ async def get_current_user(sessionID: str = Header(..., convert_underscores=Fals
         raise credentials_exception
 
 
-
 @app.post("/token", response_model=dict)
 async def login(username: str, password: str):
     # Verify username and password (not shown for simplicity)
     if any(user["username"] == username for user in fake_users_db):
         # Assume verification is successful, create a token
         token = create_token({"sub": username, "admin": False})
-        return {"access_token": token, "token_type": "bearer"}
+
+        response = {"access_token": token, "token_type": "bearer"}
+
+        # Set the cookie
+        expires = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        response.set_cookie(
+            key="Authorization",
+            value=f"Bearer {token}",
+            expires=expires,
+            httponly=True,
+            max_age=ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+            secure=False,  # Set it to True if your app is served over HTTPS
+            samesite="None",  # Set it to "Lax" or "Strict" if your app is served over HTTPS
+        )
+
+        return response
     else:
         raise HTTPException(status_code=400, detail="Username doesn't exist")
-
-
 @app.post("/register", response_model=dict)
 async def register(username: str, password: str):
 
